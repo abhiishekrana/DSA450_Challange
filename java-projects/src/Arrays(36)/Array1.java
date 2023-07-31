f6d92bd0819d398971a5d8c983460394d5079a19
Abhishek <abhishekrana4a0@gmail.com>
2023-07-31 20:30:37 +0530

    array added

--- java-projects/src/Arrays(36)/Array1.java
+++ java-projects/src/Arrays(36)/Array1.java
@@ -1,5 +1,17 @@
-public class Array1 {
-    public static void main(String[] args){
-        System.out.println();
-    }
+public class Array1
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int [] arr = {10,20,30,40,50};
+		int [] arr1=new int[arr.length];
+		int i=0;
+		for(int j=arr.length-1;i>=0;i--){
+		    arr1[i]=arr[j];
+		    i++;
+		}
+		for(int k:arr1){
+		    System.out.print(k+ " ");
+		}
+		
+	}
 }
--- java-projects/src/Arrays(36)/Array2.java
+++ java-projects/src/Arrays(36)/Array2.java
@@ -1,3 +1,16 @@
 public class Array2 {
-    
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int arr[] = {3, 5, 4, 1, 9};
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		for(int i=0;i<arr.length;i++){
+		    if(max<=arr[i])max=arr[i];
+		    else if(min>arr[i])min=arr[i];
+		}
+		System.out.print("max ->"+max+" min->"+min);
+	}
 }
+
+
