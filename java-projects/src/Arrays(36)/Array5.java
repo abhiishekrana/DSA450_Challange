0ca4b2ee9ee5c6b9443c29373a248332739cca5e
Abhishek <abhishekrana4a0@gmail.com>
2023-08-06 19:53:00 -0700

    add union and intersectgion

---
+++ java-projects/src/Arrays(36)/Array5.java
@@ -0,0 +1,36 @@
+
+// 2.Move all the negative elements to one side of the array
+public class Array4 {
+    static void rearrange(int arr[], int n)
+	{
+		int j = 0, temp;
+		for (int i = 0; i < n; i++) {
+			if (arr[i] < 0) {
+				if (i != j) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+					System.out.print("j->"+j);
+				}
+				j++;
+			}
+		}
+	}
+
+	// A utility function to print an array
+	static void printArray(int arr[], int n)
+	{
+		for (int i = 0; i < n; i++)
+			System.out.print(arr[i] + " ");
+	}
+
+	// Driver code
+	public static void main(String args[])
+	{
+		int arr[] = { -1, 2, -3, 4, 5, 6, -7, 8, 9 };
+		int n = arr.length;
+
+		rearrange(arr, n);
+		printArray(arr, n);
+	}
+}
